f26d09d8fc7eda4997341c5556d843f327b4cbde
Niklas Brünker <niklas.bruenker@gmail.com>
2023-07-27 21:36:41 +0200

    Rename remaining classes

--- src/main/java/com/smarthome/SmartHomeApplication.java
+++ src/main/java/com/smarthome/SmartHomeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatzApplication {
+public class SmartHomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatzApplication.class, args);
+		SpringApplication.run(SmartHomeApplication.class, args);
 	}
 
 }
