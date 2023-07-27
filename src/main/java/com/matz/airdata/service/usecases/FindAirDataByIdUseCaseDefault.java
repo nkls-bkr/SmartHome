e76009592b58df052734c3ab1bb628c464c69569
Niklas Brünker <niklas.bruenker@gmail.com>
2023-07-27 17:51:51 +0200

    Add Service annotation

--- src/main/java/com/matz/airdata/service/usecases/FindAirDataByIdUseCaseDefault.java
+++ src/main/java/com/matz/airdata/service/usecases/FindAirDataByIdUseCaseDefault.java
@@ -3,10 +3,12 @@ package com.matz.airdata.service.usecases;
 import com.matz.airdata.AirData;
 import com.matz.airdata.repository.AirDataRepositoryPort;
 import com.matz.airdata.service.usecases.api.FindAirDataByIdUseCase;
+import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.Optional;
 
+@Service
 public class FindAirDataByIdUseCaseDefault implements FindAirDataByIdUseCase {
     public FindAirDataByIdUseCaseDefault(AirDataRepositoryPort airDataRepositoryPort) {
         this.airDataRepositoryPort = Objects.requireNonNull(airDataRepositoryPort,
--- src/main/java/com/matz/airdata/service/usecases/SaveAirDataUseCaseDefault.java
+++ src/main/java/com/matz/airdata/service/usecases/SaveAirDataUseCaseDefault.java
@@ -3,7 +3,9 @@ package com.matz.airdata.service.usecases;
 import com.matz.airdata.AirData;
 import com.matz.airdata.repository.AirDataRepositoryPort;
 import com.matz.airdata.service.usecases.api.SaveAirDataUseCase;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SaveAirDataUseCaseDefault implements SaveAirDataUseCase {
     private final AirDataRepositoryPort airDataRepositoryPort;
 
