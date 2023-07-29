4cfde4892d23ae744d8f650c5cf7e6a823c03970
Niklas Brünker <niklas.bruenker@gmail.com>
2023-07-29 20:41:06 +0200

    Add timestamp to airdata

--- src/main/java/com/smarthome/domain/usecase/airdata/SaveAirDataUseCaseDefault.java
+++ src/main/java/com/smarthome/domain/usecase/airdata/SaveAirDataUseCaseDefault.java
@@ -1,9 +1,11 @@
 package com.smarthome.domain.usecase.airdata;
 
 import com.smarthome.domain.model.airdata.AirData;
+import com.smarthome.domain.model.airdata.AirDataBuilder;
 import com.smarthome.domain.ports.driven.AirDataRepositoryPort;
 import com.smarthome.domain.ports.driving.airdata.SaveAirDataUseCase;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class SaveAirDataUseCaseDefault implements SaveAirDataUseCase {
@@ -17,6 +19,13 @@ public class SaveAirDataUseCaseDefault implements SaveAirDataUseCase {
 
   @Override
   public AirData execute(AirData airData) {
-    return airDataRepositoryPort.save(airData);
+    AirData airDataToSave = new AirDataBuilder()
+            .setId(airData.getId())
+            .setTemperature(airData.getTemperature())
+            .setHumidity(airData.getHumidity())
+            .setLocalDateTime(LocalDateTime.now())
+            .createAirData();
+
+    return airDataRepositoryPort.save(airDataToSave);
   }
 }
